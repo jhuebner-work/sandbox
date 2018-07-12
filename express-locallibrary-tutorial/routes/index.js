a598ea1a264d8b6354cf1c7b675c8e0816f0f871
Huebner, John <John.Huebner@hmhco.com>
2018-07-12 12:04:37 -0400

    get promises and metadata working

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -1,25 +1,39 @@
 var request = require('request');
+var rp = require('request-promise');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var message = "no message";
 
-
+/* GET home page. *
+function myCallback(error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:',  response.statusCode); // Print the response status code if a response was received
+    message = response.statusCode;
+    message = "inside";
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('message:', message); // Print the HTML for the Google homepage.
+}*/
 
 router.get('/', function(req, res, next) {
-    var message = "no message";
-    var resp = request('http://www.google.com?q=test', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:',  response.statusCode); // Print the response status code if a response was received
-        message = response.statusCode;
-        message = "inside";
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        console.log('message:', message); // Print the HTML for the Google homepage.
-    });
-    console.log('resp:', resp); // Print the HTML for the Google homepage.
-    console.log('statusCode2:',  resp.statusCode); // Print the response status code if a response was received
+    var options = {
+        method: 'GET',
+        uri: 'http://www.google.com',
+        resolveWithFullResponse: true    //  <---  <---  <---  <---
+    };
 
-    res.render('index', { title: message });
+    rp(options).then(function(response) {
+            console.log('respPromise.response:', response); // Print the HTML for the Google homepage.
+            console.log('respPromise.response.statusCode:', response.statusCode); // Print the response status code if a response was received
+            message = response.statusCode;
+            res.render('index', { title: message });
+        }).catch (
+        function(response) {
+            console.log('error:');
+            res.render('index', { title: 'error'+response});
+        });
+
+
 });
 
 module.exports = router;
